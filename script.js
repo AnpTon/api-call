4e2d0526907e57ec209501bb48a6a839f90c532c
AnpTon <anpton0713@gmail.com>
2024-11-06 23:16:08 +0800

    button

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const apiUrl = 'https://meowfacts.herokuapp.com/';
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    const catFactElement = document.getElementById('catFact');
-    catFactElement.innerText = data.data[0];
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-
+function fetchCatFact() {
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        const catFactElement = document.getElementById('catFact');
+        catFactElement.innerText = data.data[0];
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+document.getElementById('Catbut').addEventListener('click', fetchCatFact);
   
