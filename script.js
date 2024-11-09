f7b65b3c1edd1b308e5f45c30d4c01b5e3d6790c
AnpTon <anpton0713@gmail.com>
2024-11-09 21:18:38 +0800

    Stylings & More Script

--- script.js
+++ script.js
@@ -15,10 +15,16 @@ async function fetchPokemon(query) {
 function displayPokemon(pokemon) {
   const pokemonInfoElement = document.getElementById('pokemonInfo');
   pokemonInfoElement.innerHTML = `
-    <h2>${pokemon.name}</h2>
-    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" style="width:150px;height:150px;">
+    <h2 class="font-bold">${pokemon.name}</h2>
+    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" class="w-24 h-24 mx-auto mb-4">
     <p>Type: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
-    <p>Abilities: ${pokemon.abilities.map(abilityInfo => abilityInfo.ability.name).join(', ')}</p>`;
+    <p>Abilities: ${pokemon.abilities.map(abilityInfo => abilityInfo.ability.name).join(', ')}</p>
+    <p>Height: ${pokemon.height / 10} m</p>
+    <p>Weight: ${pokemon.weight / 10} kg</p>
+    <p>Base Stats:</p>
+    <ul>
+      ${pokemon.stats.map(stat => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
+    </ul>`;
 }
 document.getElementById('Search').addEventListener('submit', function(event) {
   event.preventDefault();
